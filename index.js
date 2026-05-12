0df6a49f2f02112b9f8446aae05dad22eb37b2b4
vrushti hindocha <vrushtihindocha@vrushtis-MacBook-Air.local>
2026-05-12 14:47:58 +0530

    Increase JSON upload limit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 app.use('/api/auth',        authRoutes);
 app.use('/api/orders',      orderRoutes);
