264e4815f4f1d23c52e75dae02bde0ffc565ee5b
vrushti hindocha <vrushtihindocha@vrushtis-MacBook-Air.local>
2026-05-19 16:02:47 +0530

    fix cors options error

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.options('*', cors());
+
 app.use(express.json({ limit: '10mb' }));
 
 app.use('/api/auth',        authRoutes);
